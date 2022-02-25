d33b2af74c46e27d5a11ed84a371badad6744a8b
Yves-Simon Zeulner <ysz@web.de>
2022-02-25 01:57:55 +0100

    Labels und EXIF mit PIL

--- Editor.py
+++ Editor.py
@@ -1,15 +1,17 @@
 import json
 import pygame
 import os
+from PIL import Image
 
 class Picture():
-    def __init__(self, url, x = 10, y = 10, width = 200, height = 140):
+    def __init__(self, url, x = 10, y = 10, width = 200, height = 140, date = None):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.ratio = width/height
         self.url = url
+        self.date = date
         if url != "":
             self.refreshMap(url)
 
@@ -33,8 +35,10 @@ class Picture():
         return self.x < mouseX < self.x + self.width and self.y < mouseY < self.y + self.height
 
     def drawRectangle(self, screen, color):
+        if self.date != None:
+            pygame.draw.rect(screen, (255, 72, 0), [self.x - 5, self.y - 5, self.width + 6, self.height + 6])
         pygame.draw.rect(screen, color, [self.x, self.y, self.width, self.height])
-    
+
     def draw(self, screen):
         screen.blit(self.img, (self.imgPos[0] + self.x, self.imgPos[1] + self.y))
 
@@ -42,10 +46,11 @@ class Picture():
 class Startbildschirm():
 
     def __init__(self):
-        fullscreen = False
+        fullscreen = True
         
         pygame.init()
         clock = pygame.time.Clock()
+        font = pygame.font.SysFont('arialnarrow', 20) 
 
         dimension = (1000,650) 
         if(not fullscreen):
@@ -55,7 +60,7 @@ class Startbildschirm():
             infoObject = pygame.display.Info()
             dimension = (infoObject.current_w, infoObject.current_h)
         
-        tmpScreen = pygame.Surface(dimension)
+        tmpScreen = pygame.Surface((dimension[0] + 200, dimension[1] + 200))
         tmpScreen.set_alpha(180)
         
         pygame.display.set_caption("Diashow - Editor")
@@ -81,11 +86,21 @@ class Startbildschirm():
         for entry in obj :
             if entry.is_file() and entry.name != ".DS_Store":
                 if not entry.name in list(data.keys()):
-                    otherImages.append(Picture(entry.name, surSortiert.get_width() + 10, surPreview.get_height() + 10))
-
+                    otherImages.append(Picture(entry.name))
         self.placePictures(otherImages, surAlle, surSortiert.get_width(), surPreview.get_height())
-
         obj.close()
+        
+        for pic in sorted+otherImages:
+            try:
+                pic.date = Image.open("pictures/" + pic.url)._getexif().get(306)
+            except AttributeError:
+                print(pic.url)
+            
+
+        #image = Image.open("pictures/A221E0C4-5D6E-4E7A-AD7C-036E0EF6C9F2.jpg")
+        image = Image.open("pictures/haha.jpg")
+        EXIF_data = image._getexif()
+        print(EXIF_data.get(306))
 
         surSortiert.fill((100, 0, 0))
         surAlle.fill((0, 100, 0))
@@ -102,6 +117,8 @@ class Startbildschirm():
         dragPos = [0, 0]
         while running: 
 
+            mouse = pygame.mouse.get_pos() 
+
             for ev in pygame.event.get(): 
                 if ev.type == pygame.KEYDOWN:
                     if ev.key == pygame.K_ESCAPE:
@@ -115,16 +132,35 @@ class Startbildschirm():
                     running = False
                 elif ev.type == pygame.MOUSEBUTTONDOWN: 
                     if ev.button == 4:
-                        toScroll += 1
-                    elif ev.button == 5:
                         toScroll -= 1
+                    elif ev.button == 5:
+                        toScroll += 1
                     else:
                         pressed = True
                 elif ev.type == pygame.MOUSEBUTTONUP: 
                     pressed = False
                     firstTime = True
-
-            mouse = pygame.mouse.get_pos() 
+                    tmpScreen.fill((255, 255, 255, 0))
+                    if dragPos != [0, 0]:
+                        if mouse[0] < surSortiert.get_width():
+                            for i, pic in enumerate(sorted):
+                                if pic.checkLocation(mouse[0], mouse[1]):
+                                    pos = i
+                                    if drawPic in otherImages:
+                                        otherImages.remove(drawPic)
+                                    else:
+                                        sorted.remove(drawPic)
+                                    sorted.insert(pos, drawPic)
+                                    self.placePictures(sorted, surSortiert)
+                        else:
+                            if mouse[1] > surPreview.get_height():
+                                drawPic.x += dragPos[0]
+                                drawPic.y += dragPos[1]
+                                if drawPic in sorted:
+                                    sorted.remove(drawPic)
+                                    otherImages.append(drawPic)
+                        dragPos[0] = 0
+                        dragPos[1] = 0
 
             if toScroll != 0:
                 if mouse[0] < surSortiert.get_width():
@@ -137,18 +173,33 @@ class Startbildschirm():
 
             if pressed:
                 for pic in sorted + otherImages:
-                    if pic.checkLocation(mouse[0], mouse[1]):
-                        if firstTime:
+                    if firstTime:
+                        if pic.checkLocation(mouse[0], mouse[1]):
                             firstTime = False
                             preview.refreshMap(pic.url)
                             surPreview.fill((0, 0, 0))
                             preview.draw(surPreview)
+
+                            text = font.render(preview.url, False, (0, 0, 0))
+                            textRect = text.get_rect()
+                            textRect.midtop = (preview.x + preview.width/2, preview.y) 
+                            pygame.draw.rect(surPreview, (255, 255, 255), textRect)
+                            surPreview.blit(text, textRect)
+                            print(pic.date)
+                            if pic.date != None:
+                                text = font.render(pic.date, False, (0, 0, 0))
+                                textRect = text.get_rect()
+                                textRect.midbottom = (preview.x + preview.width/2, preview.y + preview.height) 
+                                pygame.draw.rect(surPreview, (255, 255, 255), textRect)
+                                surPreview.blit(text, textRect)
+                            
                             dragStart = mouse
-                        else:
-                            dragPos[0] = mouse[0] - dragStart[0]
-                            dragPos[1] = mouse[1] - dragStart[1]
-                            tmpScreen.fill((255, 255, 255, 0))
-                            pic.draw(tmpScreen)
+                            drawPic = pic
+                    else:
+                        dragPos[0] = mouse[0] - dragStart[0]
+                        dragPos[1] = mouse[1] - dragStart[1]
+                        tmpScreen.fill((255, 255, 255, 0))
+                        drawPic.draw(tmpScreen)
 
             screen.fill((0, 0, 0)) 
 
